c2ba297b579924ac172b293d80097257bf918bd3
Anthony Allen <Anthony_Allen@comcast.com>
2022-03-19 10:10:27 -0400

    createScaleFromSteps

--- src/ScaleConstructor.hs
+++ src/ScaleConstructor.hs
@@ -7,23 +7,26 @@ import Euterpea
 type ChordType = String -- "M", "m", "a", "d"
 
 -- Generate Scales from Steps and Key
+createScaleFromSteps :: Music a -> [AbsPitch] -> [Music a]
+createScaleFromSteps root = map (`transpose` root)
+
 createMajScale :: Music a -> [Music a]
-createMajScale root = map (`transpose` root) majScaleSteps
+createMajScale root = createScaleFromSteps root majScaleSteps
   where
     majScaleSteps = [0, 2, 4, 5, 7, 9, 11, 12]
 
 createMinScale :: Music a -> [Music a]
-createMinScale root = map (`transpose` root) minScaleSteps
+createMinScale root = createScaleFromSteps root minScaleSteps
   where
     minScaleSteps = [0, 2, 3, 5, 7, 8, 10, 11, 12]
 
 createWholeToneScale :: Music a -> [Music a]
-createWholeToneScale root = map (`transpose` root) wtSteps
+createWholeToneScale root = createScaleFromSteps root wtSteps
   where
     wtSteps = [0 .. 12]
 
 createRandomScale :: Music a -> [Music a]
-createRandomScale root = map (`transpose` root) randomSteps
+createRandomScale root = createScaleFromSteps root randomSteps
   where
     -- TODO: Figure out some way to do some random stuff here?!
     randomSteps = [0, 2 .. 20]
