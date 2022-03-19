e3f66dabb87893f063b8337a8f79e6788d89b0f4
Anthony Allen <Anthony_Allen@comcast.com>
2022-03-19 13:25:59 -0400

    Add Exercises Module for HSoM exercises

---
+++ src/Exercises.hs
@@ -0,0 +1,14 @@
+module Exercises where
+
+applyEach :: [a -> b] -> a -> [b]
+applyEach fns x = map ($ x) fns
+
+applyAll :: [a -> a] -> a -> a
+applyAll = foldr (.) id
+
+-- Examples
+fns0 :: [Integer -> Integer]
+fns0 = [(+ 10), (+ 5), (+ 20)]
+
+fns1 :: [String -> String]
+fns1 = [(++ "!"), (++ "?"), (++ "!"), (++ "?")]
