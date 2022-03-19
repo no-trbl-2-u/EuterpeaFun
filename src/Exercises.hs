523a9c1b83cbc361879c1eca8efaf2129d0bd7c0
Anthony Allen <Anthony_Allen@comcast.com>
2022-03-19 13:26:30 -0400

    Add comment

--- src/Exercises.hs
+++ src/Exercises.hs
@@ -1,12 +1,13 @@
 module Exercises where
 
+-- Currying exercises
 applyEach :: [a -> b] -> a -> [b]
 applyEach fns x = map ($ x) fns
 
 applyAll :: [a -> a] -> a -> a
 applyAll = foldr (.) id
 
--- Examples
+--  -- Examples
 fns0 :: [Integer -> Integer]
 fns0 = [(+ 10), (+ 5), (+ 20)]
 
