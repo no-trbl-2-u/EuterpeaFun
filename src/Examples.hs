e61cbe105118f8e39d82661e623f57214371a083
Anthony Allen <Anthony_Allen@comcast.com>
2022-03-28 11:52:11 -0400

    Refactor modules a bit.

--- src/Examples.hs
+++ src/Examples.hs
@@ -2,10 +2,12 @@ module Examples where
 
 import ChordConstructor
 import Euterpea
+import Helpers (choose, randomize)
+import PatternGenerator (pGen, pGen2)
 import ScaleConstructor
+import System.Random.SplitMix (SMGen, mkSMGen)
 
--- ------------------------ Triad Examples ------------------------
--- Chord Progression Examples
+-- ------------------------ Triad Progression Examples ------------------------
 -- Key of C Maj -> (I : IV : ii : V)
 chordProg1 :: Music Pitch
 chordProg1 =
@@ -23,24 +25,20 @@ chordProg2 =
     :+: minor (f 3 hn)
 
 playTriadExample :: IO ()
-playTriadExample =
-  do
-    play $
-      chordProg1 :+: chordProg1 :+: chordProg2 :+: major (c 4 wn)
+playTriadExample = do
+  play $ chordProg1 :+: chordProg1 :+: chordProg2 :+: major (c 4 wn)
 
 -- ------------------------ Triad Set Examples ------------------------
 cMajorHarmonyExample :: IO ()
 cMajorHarmonyExample = do
-  play $
-    melody :=: harmony
+  play $ melody :=: harmony
   where
     melody = line $ majTriadSet (c 4 qn)
     harmony = chord $ majTriadSet (c 3 qn)
 
 composedPitchSet :: IO ()
 composedPitchSet = do
-  play $
-    line pitchSet
+  play $ line pitchSet
   where
     pitchSet = majTriadSet (c 4 qn) ++ majTriadSet (d 2 qn)
 
@@ -57,19 +55,17 @@ bMajorInversionsExample = do
 -- ------------------------ Scale Set Examples ------------------------
 playBMinScale :: IO ()
 playBMinScale = do
-  play $
-    melody :+: harmony
+  play $ melody :+: harmony
   where
-    melody = line $ createMinScale (b 2 qn)
-    harmony = chord $ minTriadSet (b 2 qn)
+    melody = line . createMinScale $ b 2 qn
+    harmony = chord . minTriadSet $ b 2 qn
 
 playDMinScaleArp :: IO ()
 playDMinScaleArp = do
-  play $
-    melUp :+: melDown
+  play $ melUp :+: melDown
   where
-    melUp = line $ createMinScale (b 2 qn)
-    melDown = line $ reverse $ createMinScale (b 2 qn)
+    melUp = line . createMinScale $ b 2 qn
+    melDown = line . reverse . createMinScale $ b 2 qn
 
 playDMinScaleArpWithRes :: IO ()
 playDMinScaleArpWithRes = do
@@ -81,3 +77,29 @@ playDMinScaleArpWithRes = do
     lowFive = chord . majTriadSet $ fs 2 hn
     melUp = line . createMinScale $ root qn
     melDown = line . reverse . createMinScale $ root qn
+
+-- ------------------------ Pattern Generator Examples ------------------------
+cMajorScale :: [AbsPitch]
+cMajorScale = [40, 42, 44, 45, 47, 49, 51, 52]
+
+cMajorScaleLess :: [AbsPitch]
+cMajorScaleLess = [40, 44, 45, 47, 49]
+
+patternSet :: [[AbsPitch]]
+patternSet = [[3, 0], [0, 5], [0, 3], [5, 0], [0, 7]]
+
+-- Play Examples
+playPGen :: IO ()
+playPGen = do
+  play $ tempo 1 (instrument Vibraphone melody)
+  where
+    pitchSpace = cMajorScaleLess ++ map (+ 12) cMajorScaleLess
+    pGenExampleSet = pGen pitchSpace patternSet 40 2 (mkSMGen 300)
+    melody = line $ map (note en) pGenExampleSet
+
+playPGen2 :: IO ()
+playPGen2 = do
+  play $ tempo 2 (instrument Xylophone melody :=: instrument Vibraphone melody2)
+  where
+    melody = pGen2 (map (+ 12) cMajorScaleLess) [hn, wn, hn, qn] 0 (mkSMGen 500)
+    melody2 = pGen2 (map (+ 24) cMajorScale) [qn, sn, qn, en] 0 (mkSMGen 600)
--- src/PatternGenerator.hs
+++ src/PatternGenerator.hs
@@ -1,12 +1,10 @@
 module PatternGenerator where
 
-import Data.List (nub)
-import Euterpea
-import System.Random (Random (randomR, randomRs), StdGen, mkStdGen, next)
-
--- import System.Random (next)
-
 -- Leverages code and Ideas from -> https://www.youtube.com/watch?v=UVcXNhgVr9o
+import Euterpea (AbsPitch, Dur, Music ((:+:)), Volume, note, rest)
+import Helpers (choose, findInsts)
+import System.Random (Random (randomR))
+import System.Random.SplitMix (SMGen, mkSMGen)
 
 type PitchSpace = [AbsPitch] -- Collection of AbsPitches
 
@@ -18,26 +16,8 @@ type DistThresh = AbsPitch
 
 type Root = AbsPitch
 
--- randomize :: StdGen -> [a] -> [a]
--- randomize sg rs =
---   let n = length rs
---       plist = take n (nub (randomRs (0, n -1) sg))
---    in map (rs !!) plist
-
-choose :: [a] -> StdGen -> (a, StdGen)
-choose [] g = error "Nothing to Choose from!"
-choose xs g =
-  let (i, g') = next g
-   in (xs !! (i `mod` length xs), g')
-
-findInsts :: PitchSpace -> Pattern -> [PatternInst]
-findInsts s pat =
-  filter (all (`elem` s)) patInsts
-  where
-    patInsts = map (\a -> map (+ a) pat) s
-
--- Randomization based on Pitch spaces and patterns
-pGen :: PitchSpace -> [Pattern] -> AbsPitch -> DistThresh -> StdGen -> Pattern
+-- Interval Pattern-based Randomization using a PitchSpace
+pGen :: PitchSpace -> [Pattern] -> AbsPitch -> DistThresh -> SMGen -> Pattern
 pGen s k root d g0 =
   pInst ++ pGen s k (last pInst) d g2
   where
@@ -48,13 +28,12 @@ pGen s k root d g0 =
         && head someInst /= root
     piNear = filter nearby patts -- which instances are nearby (within d half steps)?
     (pInst, g2) =
-      -- pick nearby instances available
       if not (null piNear)
-        then choose piNear g1
+        then choose piNear g1 -- pick nearby instances available
         else choose patts g1 -- no nearby instances available
 
 -- LESS complex randomization, but randomize duration
-pGen2 :: [AbsPitch] -> [Dur] -> Int -> StdGen -> Music (AbsPitch, Volume)
+pGen2 :: [AbsPitch] -> [Dur] -> Int -> SMGen -> Music (AbsPitch, Volume)
 pGen2 pitches durs thresh g0 =
   let (p, g1) = choose pitches g0
       (d, g2) = choose durs g1
@@ -64,37 +43,3 @@ pGen2 pitches durs thresh g0 =
           then rest d
           else note d (p, v)
    in x :+: pGen2 pitches durs thresh g3
-
--- Example values
-cMajorScale :: PitchSpace
-cMajorScale = scaleOctDown ++ scale ++ scaleOctUp
-  where
-    scale = [40, 42, 44, 45, 47, 49, 51, 52]
-    scaleOctDown = map (\n -> n - 12) scale
-    scaleOctUp = map (+ 12) scale
-
-exampleSet :: Pattern
-exampleSet = pGen pitchSpace patterns root distThresh seed
-  where
-    pitchSpace = cMajorScale
-    patterns = [[3, 0], [0, 5], [0, 3], [5, 0], [0, 7]] :: [Pattern]
-    distThresh = 2 :: DistThresh
-    root = 40 :: Root
-    seed = mkStdGen 6 :: StdGen
-
-pGenExample :: Music AbsPitch
-pGenExample = line $ map (note $ fst duration) exampleSet
-  where
-    duration = choose [sn, sn, en, qn] (mkStdGen 500)
-
--- Play Examples
-playPGen :: IO ()
-playPGen = do
-  play $ tempo 1 (instrument Vibraphone pGenExample)
-
-playPGen2 :: IO ()
-playPGen2 = do
-  play $ tempo 2 (instrument Vibraphone melody :=: instrument Vibraphone melody2)
-  where
-    melody = pGen2 cMajorScale [hn, wn, hn, qn] 0 (mkStdGen 500)
-    melody2 = pGen2 (map (+ 12) cMajorScale) [qn, sn, qn, en] 0 (mkStdGen 600)
